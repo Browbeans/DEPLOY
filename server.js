688687c2dcb34a7861a451618225f5410e87d740
Oliver Johansson <oliverjohansson3@gmail.com>
2021-05-31 09:17:13 +0200

    Boilerplate

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello Woreld')
+})
+
+app.listen(port, () => {
+    console.log('server is running on: ', port)
+})
